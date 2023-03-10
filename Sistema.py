3277aae2b5162dbe28dc2c174c2532bc4c3a396c
YordiOrtiz <yordiortiz09@outlook.com>
2023-03-10 14:27:31 -0600

    sensores

--- Sistema.py
+++ Sistema.py
@@ -1,7 +1,8 @@
 import RPi.GPIO as GPIO
-from ultrasonico import Ultrasonico
+import Ultrasonico
 from Temperatura import TemperaturaHumedad
-from led import Led
+import Temperatura
+import Led
 
 class Sistema:
     def __init__(self, led_pins, ultrasonico_pins, temp_hum_pin):
