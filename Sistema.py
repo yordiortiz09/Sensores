81a897fc00071f01ffe3066e0e36dc23753c1890
YordiOrtiz <yordiortiz09@outlook.com>
2023-03-13 12:53:07 -0600

    rasp

--- Sistema.py
+++ Sistema.py
@@ -1,53 +1,15 @@
 import RPi.GPIO as GPIO
 from ultrasonico import Ultrasonico
-from Temperatura import TemperaturaHumedad
-from led import Led
 
-class Sistema:
-    def __init__(self, led_pins, ultrasonico_pins, temp_hum_pin):
-        self.leds = [Led(pin) for pin in led_pins]
-        self.ultrasonicos = [Ultrasonico(pin[0], pin[1]) for pin in ultrasonico_pins]
-        self.temp_hum = TemperaturaHumedad(temp_hum_pin)
-        
-    def configurar_sensores(self):
-        for led in self.leds:
-            led.configurar_pin()
-        for ultrasonico in self.ultrasonicos:
-            ultrasonico.configurar_pin()
-        self.temp_hum.configurar_pin()
-        
-    def leer_led(self, num_led):
-        if num_led >= 0 and num_led < len(self.leds):
-            return GPIO.input(self.leds[num_led].pin)
-        else:
-            return None
-        
-    def encender_led(self, num_led):
-        if num_led >= 0 and num_led < len(self.leds):
-            self.leds[num_led].encender()
-            
-    def apagar_led(self, num_led):
-        if num_led >= 0 and num_led < len(self.leds):
-            self.leds[num_led].apagar()
-            
-    def medir_distancia(self, num_ultrasonico):
-        if num_ultrasonico >= 0 and num_ultrasonico < len(self.ultrasonicos):
-            return self.ultrasonicos[num_ultrasonico].medir_distancia()
-        else:
-            return None
-        
-    def medir_temperatura_y_humedad(self):
-        return self.temp_hum.med
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
 
-led_pins = [4]
-ultrasonico_pins = [(23, 24), (5, 6), (12, 16)]
-temp_hum_pin = 18
+trigger_pin = 23
+echo_pin = 24
 
-sistema = Sistema(led_pins, ultrasonico_pins, temp_hum_pin)
-sistema.configurar_sensores()
+sensor = Ultrasonico(trigger_pin, echo_pin)
 
-distancia = sistema.medir_distancia(0)
-temp_hum = sistema.medir_temperatura_y_humedad()
+distancia = sensor.medir_distancia()
+print("Distancia: %.2f cm" % distancia)
 
-print(distancia)
-print(temp_hum)
+del sensor
--- Ultrasonico.py
+++ Ultrasonico.py
@@ -5,19 +5,17 @@ class Ultrasonico:
     def __init__(self, trigger_pin, echo_pin):
         self.trigger_pin = trigger_pin
         self.echo_pin = echo_pin
-        self.velocidad_sonido = 34300  
-        self.tiempo_max = 0.04  
+        self.velocidad_sonido = 34300  # cm/s
+        self.tiempo_max = 0.04  # s
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.trigger_pin, GPIO.OUT)
         GPIO.setup(self.echo_pin, GPIO.IN)
 
     def medir_distancia(self):
-        # enviar pulso de sonido
         GPIO.output(self.trigger_pin, True)
         time.sleep(0.00001)
         GPIO.output(self.trigger_pin, False)
-        
-     
+
         tiempo_inicio = time.time()
         while GPIO.input(self.echo_pin) == 0:
             if time.time() - tiempo_inicio > self.tiempo_max:
@@ -27,12 +25,10 @@ class Ultrasonico:
             if time.time() - tiempo_inicio > self.tiempo_max:
                 return None
         tiempo_fin = time.time()
-        
-     
+
         duracion = tiempo_fin - tiempo_inicio
         distancia = (duracion * self.velocidad_sonido) / 2
         return distancia
 
-    def configurar_pin(self):
-        GPIO.setup(self.trigger_pin, GPIO.OUT)
-        GPIO.setup(self.echo_pin, GPIO.IN)
+    def __del__(self):
+        GPIO.cleanup()
